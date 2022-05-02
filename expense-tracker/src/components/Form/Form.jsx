b3e59c93b0a9f290d094223649f5931e87553453
Glauco Lustosa <glauco.lustosa@icloud.com>
2022-05-02 13:58:18 -0300

    Form Data

--- expense-tracker/src/components/Form/Form.jsx
+++ expense-tracker/src/components/Form/Form.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FormControl, InputLabel, MenuItem, Grid, Button, Select, TextField, Typography } from '@material-ui/core';
 import useStyles from './styles'
+
+const initialState = {
+    amount: '',
+    category: '',
+    type: 'Income',
+    date: new Date()
+}
 function Form() {
     const classes = useStyles()
+    const [formData, setFormData] = useState(initialState)
+    console.log(formData);
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -17,7 +26,8 @@ function Form() {
                     <InputLabel>
                         Type
                     </InputLabel>
-                    <Select>
+                    <Select value={formData.type}
+                        onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
                         <MenuItem value='Income'>
                             Income
                         </MenuItem>
@@ -31,7 +41,10 @@ function Form() {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
-                    <Select>
+                    <Select
+                        value={formData.category}
+                        onChange={(e) => setFormData({ ...formData, category: e.target.value })}
+                    >
                         <MenuItem value='business'>Business</MenuItem>
                         <MenuItem value='salary'>Salary</MenuItem>
                     </Select>
@@ -41,7 +54,10 @@ function Form() {
                 <TextField
                     type='number'
                     label='Amount'
-                    fullWidth>
+                    fullWidth
+                    value={formData.amount}
+                    onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+                >
 
                 </TextField>
             </Grid>
@@ -49,7 +65,10 @@ function Form() {
                 <TextField
                     type='date'
                     label='Date'
-                    fullWidth>
+                    fullWidth
+                    value={formData.date}
+                    onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+                >
 
                 </TextField>
             </Grid>
